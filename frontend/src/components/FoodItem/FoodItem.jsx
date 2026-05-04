bf3d5c02b60d7255ad54e73bd34f260c97d5bac1
alexngumbau <alex.ngumbau@equitybank.co.ke>
2026-05-04 10:45:03 +0300

    created the counter functionality for adding or subtracting items to be submitted

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import { assets } from '../../assets/frontend_assets/assets'
 import './FoodItem.css'
 
 const FoodItem = ({id, name, price, description, image}) => {
+
+  const [itemCount, setitemCount ] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} alt="" className="food-item-image" />
+        {
+          !itemCount
+          ? <img className='add' onClick={() => setitemCount(prev => prev + 1)} src={assets.add_icon_white} alt="" />
+          : <div className="food-item-counter">
+            <img onClick={() => setitemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="" />
+            <p>{itemCount}</p>
+            <img onClick={() => setitemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" />
+          </div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
