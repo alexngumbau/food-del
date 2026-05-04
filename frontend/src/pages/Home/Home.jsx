8d0aaa57fc9ab49b361bf55e443b334ff75d94fb
alexngumbau <alex.ngumbau@equitybank.co.ke>
2026-05-04 15:20:44 +0300

    completed the app download section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import Header from '../../components/Header/Header'
 import './Home.css'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import Footer from '../../components/Footer/Footer'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -14,6 +14,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   )
 }
