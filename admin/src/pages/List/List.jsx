d8a16840b7c809baf0688a53486b49c4b7ba517c
alexngumbau <alex.ngumbau@equitybank.co.ke>
2026-05-08 11:14:52 +0300

    completed the list component

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-import React from 'react'
+export const List = ({url}) => {
 
-export const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);  
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  }
+
+  const removeFood = async(foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`, {id:foodId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+    } else {
+      toast.error("Error");
+    }
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    fetchList();
+  }, []);
+
   return (
-    <div>List</div>
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>x</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
