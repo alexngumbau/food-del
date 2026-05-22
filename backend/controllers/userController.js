3f5e9f9715243eee95c7990fa1dff63d98c3f001
alexngumbau <alex.ngumbau@equitybank.co.ke>
2026-05-22 11:31:47 +0300

    implemented functionality to refresh the token before it expires in the admin panel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,7 +20,7 @@ const loginUser = async (req,res) => {
       return res.json({success: false, message: "Invalid email or password"});
     }
 
-    const accessToken = createToken(user._id, user.role, "6m");
+    const accessToken = createToken(user._id, user.role, "1m");
     const refreshToken = createToken(user._id, user.role, "7d");
 
     res.json({success: true, token: accessToken, refreshToken});
@@ -48,7 +48,7 @@ const adminLogin = async (req, res) => {
       return res.json({success: false, message: "Access denied. Admins only."});
     }
 
-    const accessToken = createToken(user._id, user.role, "6m");
+    const accessToken = createToken(user._id, user.role, "1m");
     const refreshToken = createToken(user._id, user.role, "7d");
 
     res.json({success: true, token: accessToken, refreshToken});
@@ -65,13 +65,15 @@ const createToken = (id, role, expiresIn) => {
 
 // refresh token
 const refreshAccessToken = (req, res) => {
+  console.log('HOOORAY I GOT CALLED');
+  
   const {refreshToken} = req.body;
   if (!refreshToken) {
     return res.json({success: false, message: "No refresh token provided"});
   }
   try {
     const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
-    const accessToken = createToken(decoded.id, decoded.role, "6m");
+    const accessToken = createToken(decoded.id, decoded.role, "1m");
     res.json({success: true, token: accessToken});
   } catch (error) {
     res.json({success: false, message: "Invalid or expired refresh token. Please login again."});
@@ -107,7 +109,7 @@ const registerUser = async(req, res) => {
     });
 
     const user = await newUser.save();
-    const accessToken = createToken(user._id, user.role, "6m");
+    const accessToken = createToken(user._id, user.role, "1m");
     const refreshToken = createToken(user._id, user.role, "7d");
 
     res.json({success: true, token: accessToken, refreshToken});
